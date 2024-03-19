a72e0482e31cf24350e4de2e004f2beff5493fa1
Abner Silva <155109509+AbnerSLima@users.noreply.github.com>
2024-03-19 12:55:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+function atualizarValorAPagar() {
+    let valorTotal = document.getElementById("valor_total").value;
+    let desconto = document.getElementById("desconto").value;
+    let valorAPagar = valorTotal - desconto;
+    let parcelas = document.getElementById("parcelas").value;
+    if (parcelas > 1) {
+        valorAPagar = valorAPagar / parcelas;
+    }
+    document.getElementById("valor_a_pagar").value = valorAPagar.toFixed(2);
+}
+
+const button_print=document.getElementById("button_print")
+
+button_print.addEventListener("click",(evt)=>{
+    window.print()
+})

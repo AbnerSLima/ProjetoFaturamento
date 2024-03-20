755466ccb192f548345f6ff1933580d8505c4b29
AbnerSLima <abnerdslima@gmail.com>
2024-03-20 17:26:24 -0300

    update script

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+/* Pagina Pedidos */
+const faturarButton = document.getElementById('faturarButton');
 
+faturarButton.addEventListener('click', function() {
+    const valorTotal = document.getElementById("valor_total_venda").value;
+    window.location.href = "faturamento.html?valor_total=" + valorTotal;
+});
+
+/* Pagina Faturamento */
 function atualizarValorAPagar() {
-    let valorTotal = document.getElementById("valor_total").value;
+    let valorTotalFaturamento = document.getElementById("valor_total").value;
     let desconto = document.getElementById("desconto").value;
-    let valorAPagar = valorTotal - desconto;
+    let valorAPagar = valorTotalFaturamento - desconto;
     let parcelas = document.getElementById("parcelas").value;
     if (parcelas > 1) {
         valorAPagar = valorAPagar / parcelas;
@@ -10,6 +18,7 @@ function atualizarValorAPagar() {
     document.getElementById("valor_a_pagar").value = valorAPagar.toFixed(2);
 }
 
+/* Pagina Romaneio */
 const button_print=document.getElementById("button_print")
 
 button_print.addEventListener("click",(evt)=>{

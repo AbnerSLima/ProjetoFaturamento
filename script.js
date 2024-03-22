e25c732148afc370f85b8a7fb50ea4405542e0b6
AbnerSLima <abnerdslima@gmail.com>
2024-03-22 12:29:04 -0300

    Update add_Itens

--- script.js
+++ script.js
@@ -3,24 +3,25 @@
 /* Pagina Produtos */
 
 /* Pagina Pedidos */
-document.addEventListener("DOMContentLoaded", function() {
 function adicionarItem() {
+    console.log('dando certo')
     const codigo = document.getElementById('cod_de_barras').value;
     const listaItens = document.getElementById('lista-itens').querySelectorAll('tbody tr');
     const listaPedidos = document.getElementById('lista_de_itens').querySelector('tbody');
 
+    var qnt1 = 1
     listaItens.forEach(item => {
         if (item.cells[0].textContent === codigo) {
             const descricao = item.cells[1].textContent;
             const valor = item.cells[2].textContent;
             const newRow = listaPedidos.insertRow();
-            newRow.insertCell(0).textContent = codigo;
-            newRow.insertCell(1).textContent = descricao;
+            newRow.insertCell(0).textContent = descricao;
+            newRow.insertCell(1).textContent = qnt1;
             newRow.insertCell(2).textContent = valor;
+            newRow.insertCell(3).textContent = valor;
         }
     });
 }
-});
 
 var valorTotalVenda = 500 /*Precisa-se criar um cauculo onde todas as */
 const valorTotalPedido = document.querySelector('#valor_total_venda')
